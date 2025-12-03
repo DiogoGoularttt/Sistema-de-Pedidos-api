15d6a5835be5af69b422b013270d2a0288f1bce6
Diogo Goulart <diogosgoulart@icloud.com>
2025-12-03 03:35:17 -0300

    :rocket: feat: Criação de usuário via API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'phone' => 'required|string|max:15',
+            'role' => 'required|in:admin,client',
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+            'phone' => $data['phone'],
+            'role' => $data['role'],
+        ]);
+
+        return response()->json([
+            'message' => 'Validação ok',
+            'data' => $data
+        ], 200);
+    }
 }
